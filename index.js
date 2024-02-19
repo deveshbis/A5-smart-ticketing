b1002da66e8f04cb8473e0b6bc89e10d8c9bcb49
deveshbis <deveshbiswas.cse@gmail.com>
2024-02-19 23:24:07 +0600

    code updated

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ for (let i = 0; i < cards.length; i++) {
 
         const TicketTag = parseFloat(document.getElementById("ticket-price").innerText.split(" ")[0]);
 
+        const myDiv = document.getElementById('myDiv');
+
         const p = document.createElement("p");
         p.innerText = title;
 
@@ -53,9 +55,17 @@ for (let i = 0; i < cards.length; i++) {
         const p3 = document.createElement("p");
         p3.innerText = TicketTag;
 
+        
+        const lineBreak = document.createElement('br');
+        
+        
+
         titleContainer.appendChild(p);
         titleContainer.appendChild(p2);
         titleContainer.appendChild(p3);
+        myDiv.appendChild(lineBreak);
+        
+        
 
         const countLeft = document.getElementById("negative").innerText;
 
